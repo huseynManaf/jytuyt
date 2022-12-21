7f2b0174321d22f7c7a0650ab412b10802d35893
[huseyn] <[huseynim@code.edu.az]>
2022-12-21 20:43:50 +0400

    'int'

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,6 +4,9 @@ namespace ConsoleApp3
 {
     class Program
     {
+        int saniye = 360000;
+        int gun = 24 * 60 * 60;
+        Console.writeline($"{gun } gun");
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
